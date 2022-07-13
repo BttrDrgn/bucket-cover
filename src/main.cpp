5cfb6fd36d507006a54d8d76206f451a95df237d
BttrDrgn <49764143+BttrDrgn@users.noreply.github.com>
2022-07-13 13:26:59 -0500

    Init commit

---
+++ src/main.cpp
@@ -0,0 +1,50 @@
+#include <sdk/SexySDK.hpp>
+#include <callbacks/callbacks.hpp>
+
+void init()
+{
+	if (MH_Initialize() == MH_OK);
+	{
+		Sexy::LogicMgr::setup();
+
+		MH_EnableHook(MH_ALL_HOOKS);
+	}
+
+	callbacks::on(callbacks::type::beginturn2, []()
+	{
+		Sexy::LogicMgr::ActivateFreeBallCover(1);
+	});
+}
+
+DWORD WINAPI OnAttachImpl(LPVOID lpParameter)
+{
+	init();
+	return 0;
+}
+
+DWORD WINAPI OnAttach(LPVOID lpParameter)
+{
+	__try
+	{
+		return OnAttachImpl(lpParameter);
+	}
+	__except (0)
+	{
+		FreeLibraryAndExitThread((HMODULE)lpParameter, 0xDECEA5ED);
+	}
+
+	return 0;
+}
+
+BOOL WINAPI DllMain(HMODULE hModule, DWORD dwReason, LPVOID lpReserved)
+{
+	switch (dwReason)
+	{
+	case DLL_PROCESS_ATTACH:
+		DisableThreadLibraryCalls(hModule);
+		CreateThread(nullptr, 0, OnAttach, hModule, 0, nullptr);
+		return true;
+	}
+
+	return false;
+}
